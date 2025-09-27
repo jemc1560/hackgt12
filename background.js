fcd95c84c3a6195968a10c4b41cbc3c1d09c5b2a
sam-i-am012 <samanthamau12@gmail.com>
2025-09-27 13:38:27 -0400

    temp working submit button

--- background.js
+++ background.js
@@ -1,20 +1,14 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes(" ")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-//event listener for when  user clicks on icon
-  chrome.action.onClicked.addListener((tab) => {
-  chrome.action.setBadgeText({text: 'ON'});
-  chrome.action.setBadgeBackgroundColor({color: '#444a63ff'});
-  
-  chrome.action.setTitle({
-    tabId: tab.id,
-    title: `You are on tab: ${tab.id}`});
+// listen for messages from the pop up 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action == "detectMisinformation") {
+    const quote = message.quote; 
+    console.log("Have received quote to verify:", quote);
+
+    // this is where to actually fact check 
+    setTimeout(() => {
+      sendResponse({result: "success"});
+    }, 1000);
+
+    return true; 
+  }
 });
